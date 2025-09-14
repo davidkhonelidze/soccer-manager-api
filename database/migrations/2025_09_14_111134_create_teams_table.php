9241fb05fa18d22f52f3bed877298f5734e4f34d
davidkhonelidze <davidkhonelidze@gmail.com>
2025-09-14 15:49:25 +0400

    team and player tables structure

---
+++ database/migrations/2025_09_14_111134_create_teams_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->unique();
+            $table->foreignId('country_id')->constrained('countries');
+            $table->decimal('balance', 15, 2)
+                ->default(config('soccer.team.initial_balance'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
